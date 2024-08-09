7a0807beda6f94217b0800621667cff14ee98077
zhongxiaoyao.zxy <zhongxiaoyao.zxy@antgroup.com>
2024-08-09 17:02:28 +0800

    add prefetch

--- src/algorithm/hnswlib/hnswalg.h
+++ src/algorithm/hnswlib/hnswalg.h
@@ -113,8 +113,6 @@ private:
     float alpha_;
     uint32_t po_;
     uint32_t pl_;
-    uint32_t p_round_;
-    float p_rate_;
 
 public:
     HierarchicalNSW(SpaceInterface* s) {
@@ -149,8 +147,6 @@ public:
           alpha_(alpha) {
         this->po_ = 1;
         this->pl_ = 1;
-        this->p_rate_ = 1;
-        this->p_round_ = 20;
 
         max_elements_ = max_elements;
         num_deleted_ = 0;
@@ -367,27 +363,22 @@ public:
         }
     }
 
-
     std::vector<int32_t>
     INT4_L2_batch(std::vector<int32_t>& n1_vec,
-                  std::vector<int32_t>& n2_vec,
+                  double norm2,
                   std::vector<const void*>& p1_vec,
-                  std::vector<const void*>& p2_vec,
-                  int dim,
-                  int size) {
+                  const void* query,
+                  int size) const {
         std::vector<int32_t> ret(size);
         for (int i = 0; i < size; i++) {
-            ret[i] = INT4_L2_precompute(n1_vec[i], n2_vec[i], p1_vec[i], p2_vec[i], dim);
+            ret[i] = INT4_L2_precompute(n1_vec[i], norm2, p1_vec[i], query, 960);
         }
         return ret;
     }
 
     inline int32_t
-    INT4_L2_precompute(int32_t norm1,
-                       int32_t norm2,
-                       const void* p1_vec,
-                       const void* p2_vec,
-                       int dim) const {
+    INT4_L2_precompute(
+        int32_t norm1, int32_t norm2, const void* p1_vec, const void* p2_vec, int dim) const {
         return norm1 + norm2 - 2 * INT4_IP_avx512_impl(p1_vec, p2_vec, dim);
     }
 
@@ -516,9 +507,9 @@ public:
             auto xx = _mm512_loadu_si512((__m512i*)(x + d));
             auto yy = _mm512_loadu_si512((__m512i*)(y + d));
 
-//            if (prefetch) {
-//                _mm_prefetch(prefetch + d, _MM_HINT_T0);
-//            }
+            //            if (prefetch) {
+            //                _mm_prefetch(prefetch + d, _MM_HINT_T0);
+            //            }
 
             if (d + 64 >= dim / 2) {
                 __m512i mask_overflow = _mm512_setr_epi32(0xffffffff,
@@ -547,17 +538,17 @@ public:
 
             sum = _mm512_add_epi16(sum, _mm512_maddubs_epi16(xx1, yy1));
             sum = _mm512_add_epi16(sum, _mm512_maddubs_epi16(xx2, yy2));
-//
-//            if (d / 64 == this->p_round_ and NB != -10000) {
-//                result = 0;
-//                _mm512_store_si512((__m512i*)temp, sum);
-//                for (int i = 0; i < 32; ++i) {
-//                    result += temp[i];
-//                }
-//                if (result + (dim / 2 - d) * 225 * this->p_rate_ < NB) {
-//                    return 0;
-//                }
-//            }
+            //
+            //            if (d / 64 == this->p_round_ and NB != -10000) {
+            //                result = 0;
+            //                _mm512_store_si512((__m512i*)temp, sum);
+            //                for (int i = 0; i < 32; ++i) {
+            //                    result += temp[i];
+            //                }
+            //                if (result + (dim / 2 - d) * 225 * this->p_rate_ < NB) {
+            //                    return 0;
+            //                }
+            //            }
         }
         result = 0;
         _mm512_store_si512((__m512i*)temp, sum);
@@ -959,8 +950,6 @@ public:
         std::vector<int> try_pls(15);
         std::iota(try_pos.begin(), try_pos.end(), 1);
         std::iota(try_pls.begin(), try_pls.end(), 1);
-        std::vector<float> try_p_rates = {0.5, 0.6, 0.7, 0.8, 0.9};
-        std::vector<uint32_t> try_p_rounds = {2, 3, 4, 5, 6, 7, 8, 9};
 
         bool have_optimized = true;
         if (have_optimized) {
@@ -974,17 +963,12 @@ public:
                 try_pos.assign({1});
                 try_pls.assign({1});
             }
-
-            this->p_rate_ = 1;
-            this->p_round_ = 20;
         }
 
         printf("=============Start optimization=============\n");
         this->ef_ = 80;
         this->po_ = 1;
         this->pl_ = 1;
-        this->p_rate_ = 1;
-        this->p_round_ = 20;
         auto st = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < sample_points_num; ++i) {
             searchKnn(getDataByInternalId(i), k);
@@ -1024,43 +1008,8 @@ public:
             best_po,
             best_pl,
             100.0 * (baseline_ela / min_ela - 1));
-
-        baseline_ela = min_ela;
-        return ;
-
-        float best_p_rate = 1;
-        int best_p_round = 20;
-        for (auto try_p_round : try_p_rounds) {
-            for (auto try_p_rate : try_p_rates) {
-                this->p_round_ = try_p_round;
-                this->p_rate_ = try_p_rate;
-                auto st = std::chrono::high_resolution_clock::now();
-                for (int i = 0; i < sample_points_num; ++i) {
-                    searchKnn(getDataByInternalId(i), k);
-                }
-                auto ed = std::chrono::high_resolution_clock::now();
-                auto ela = std::chrono::duration<double>(ed - st).count();
-                if (ela < min_ela) {
-                    min_ela = ela;
-                    best_p_rate = try_p_rate;
-                    best_p_round = try_p_round;
-                }
-                printf("try p_rate = %.2f, p_round = %d, gaining %.2f%% improvement\n",
-                       try_p_rate,
-                       try_p_round,
-                       100.0 * (baseline_ela / ela - 1));
-            }
-        }
-        this->p_round_ = best_p_round;
-        this->p_rate_ = best_p_rate;
-
-        printf(
-            "settint best p_rate = %.2f, best p_round = %d\n"
-            "gaining %.2f%% performance improvement\n"
-            "=============Done optimization=============\n",
-            best_p_rate,
-            best_p_round,
-            100.0 * (baseline_ela / min_ela - 1));
+        this->po_ = best_po;
+        this->pl_ = best_pl;
     }
 
     inline void
@@ -1267,11 +1216,8 @@ public:
         float dist;
         auto* codes = get_encoded_data(ep_id, code_size + 8);
         if (sq_num_bits_ == 8) {
-            dist = INT8_L2(((int64_t*)(codes + code_size)),
-                           norm2,
-                           (const void*)codes,
-                           transformed_query,
-                           dim);
+            dist = INT8_L2(
+                ((int64_t*)(codes + code_size)), norm2, (const void*)codes, transformed_query, dim);
         } else if (sq_num_bits_ == 4) {
             dist = INT4_L2_precompute(
                 *((int64_t*)(codes + code_size)), norm2, codes, transformed_query, dim);
@@ -1287,13 +1233,6 @@ public:
         uint32_t hops = 0;
         uint32_t dist_cmp = 0;
         std::vector<int> to_be_visited(M_ * 2);
-        int batch_size = 4;
-
-//        std::vector<int32_t> dist_vec(batch_size);
-//        std::vector<int32_t> n1_vec(batch_size);
-//        std::vector<int32_t> n2_vec(batch_size);
-//        std::vector<const void*> p1_vec(batch_size);
-//        std::vector<const void*> p2_vec(batch_size);
 
         while (!candidate_set.empty()) {
             hops++;
@@ -1309,6 +1248,13 @@ public:
                 current_node_pair, next_node_pair, visited_array, visited_array_tag, to_be_visited);
             dist_cmp += count_no_visited;
 
+            for (size_t j = 0; j < this->po_; j++) {
+                auto vector_data_ptr = (uint8_t*)get_encoded_data(to_be_visited[j], code_size + 8);
+#ifdef USE_SSE
+                mem_prefetch(vector_data_ptr, this->pl_);
+#endif
+            }
+
             for (size_t j = 0; j < count_no_visited; j++) {
                 int candidate_id = to_be_visited[j];
                 if (j + this->po_ <= count_no_visited) {
@@ -1318,12 +1264,6 @@ public:
                     mem_prefetch(vector_data_ptr, this->pl_);
 #endif
                 }
-//                for (int k = 0; k < batch_size; k++) {
-//                }
-//                INT4_L2_batch(n1_vec, n2_vec, p1_vec, p2_vec, batch_size);
-//                for (int k = 0; k < batch_size; k++) {
-//
-//                }
                 auto* codes = get_encoded_data(candidate_id, code_size + 8);
                 if (sq_num_bits_ == 8) {
                     dist = INT8_L2(((int64_t*)(codes + code_size)),
--- src/algorithm/hnswlib/hnswlib.h
+++ src/algorithm/hnswlib/hnswlib.h
@@ -323,10 +323,10 @@ public:
         throw std::runtime_error("un-support int4 transform");
     }
 
-//    virtual int32_t
-//    INT4_IP_avx512_impl(const void* p1_vec, const void* p2_vec, int dim) const {
-//        throw std::runtime_error("un-support int4 transform");
-//    }
+    //    virtual int32_t
+    //    INT4_IP_avx512_impl(const void* p1_vec, const void* p2_vec, int dim) const {
+    //        throw std::runtime_error("un-support int4 transform");
+    //    }
 
     virtual int32_t
     INT4_IP_impl(const void* p1_vec, const void* p2_vec, int dim) const {

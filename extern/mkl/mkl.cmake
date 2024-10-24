6c22a3fa1f40f6e020e2eec06f9f1bff75f9bbee
Coien-rr <972054003@qq.com>
2024-10-24 15:09:22 +0800

    fix(extern): remove duplicate mkl_core path

--- extern/mkl/mkl.cmake
+++ extern/mkl/mkl.cmake
@@ -49,7 +49,6 @@ if (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64" AND ENABLE_INTEL_MKL)
         ${MKL_PATH}/libmkl_avx2.so
         ${MKL_PATH}/libmkl_mc3.so
         ${MKL_PATH}/libmkl_gf_lp64.so
-        ${MKL_PATH}/libmkl_core.so
         ${MKL_PATH}/libmkl_intel_thread.so
         ${OMP_PATH}/libiomp5.so
     )
@@ -60,9 +59,9 @@ if (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64" AND ENABLE_INTEL_MKL)
     message ("enable intel-mkl as blas backend")
 else ()
     set(BLAS_LIBRARIES libopenblas.a)
-    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang") 
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         list(PREPEND BLAS_LIBRARIES omp)
-    else() 
+    else()
         list(PREPEND BLAS_LIBRARIES gomp)
     endif()
     message ("enable openblas as blas backend")

0781e3dbfdb35bc39a08961ec65b06b35e3a3cc6
LHT129 <tianlan.lht@antgroup.com>
2024-10-28 03:50:49 +0000

    introduce hgraph
    - add basic implement for hgraph(hnswsq version)
    - add one test for it
    - fix HGraphParameters trans error
    
    Signed-off-by: LHT129 <tianlan.lht@antgroup.com>

---
+++ src/index/hgraph_index.h
@@ -0,0 +1,283 @@
+
+// Copyright 2024-present the vsag project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <random>
+#include <shared_mutex>
+
+#include "../utils.h"
+#include "ThreadPool.h"
+#include "algorithm/hnswlib/algorithm_interface.h"
+#include "algorithm/hnswlib/visited_list_pool.h"
+#include "common.h"
+#include "data_cell/flatten_interface.h"
+#include "data_cell/graph_interface.h"
+#include "index_common_param.h"
+#include "typing.h"
+#include "vsag/index.h"
+
+namespace vsag {
+class HGraphIndex : public Index {
+public:
+    struct CompareByFirst {
+        constexpr bool
+        operator()(std::pair<float, InnerIdType> const& a,
+                   std::pair<float, InnerIdType> const& b) const noexcept {
+            return a.first < b.first;
+        }
+    };
+
+    using MaxHeap = std::priority_queue<std::pair<float, InnerIdType>,
+                                        Vector<std::pair<float, InnerIdType>>,
+                                        CompareByFirst>;
+
+    HGraphIndex(const nlohmann::json& index_param, const IndexCommonParam& common_param) noexcept;
+
+    void
+    Init();
+
+    tl::expected<std::vector<int64_t>, Error>
+    Build(const DatasetPtr& data) override {
+        SAFE_CALL(return this->build(data));
+    }
+
+    tl::expected<std::vector<int64_t>, Error>
+    Add(const DatasetPtr& data) override {
+        SAFE_CALL(return this->add(data));
+    }
+
+    tl::expected<DatasetPtr, Error>
+    KnnSearch(const DatasetPtr& query,
+              int64_t k,
+              const std::string& parameters,
+              BitsetPtr invalid = nullptr) const override {
+        auto func = [&invalid](int64_t id) -> bool {
+            if (invalid == nullptr) {
+                return false;
+            }
+            int64_t bit_index = id & ROW_ID_MASK;
+            return invalid->Test(bit_index);
+        };
+        SAFE_CALL(return this->knn_search(query, k, parameters, func);)
+    }
+
+    tl::expected<DatasetPtr, Error>
+    KnnSearch(const DatasetPtr& query,
+              int64_t k,
+              const std::string& parameters,
+              const std::function<bool(int64_t)>& filter) const override {
+        SAFE_CALL(return this->knn_search(query, k, parameters, filter);)
+    }
+
+    // TODO(LHT): implement
+    tl::expected<DatasetPtr, Error>
+    RangeSearch(const DatasetPtr& query,
+                float radius,
+                const std::string& parameters,
+                int64_t limited_size = -1) const override {
+        return Dataset::Make();
+    }
+
+    // TODO(LHT): implement
+    tl::expected<DatasetPtr, Error>
+    RangeSearch(const DatasetPtr& query,
+                float radius,
+                const std::string& parameters,
+                BitsetPtr invalid,
+                int64_t limited_size = -1) const override {
+        return Dataset::Make();
+    }
+
+    // TODO(LHT): implement
+    tl::expected<DatasetPtr, Error>
+    RangeSearch(const DatasetPtr& query,
+                float radius,
+                const std::string& parameters,
+                const std::function<bool(int64_t)>& filter,
+                int64_t limited_size) const override {
+        return Dataset::Make();
+    }
+
+    tl::expected<float, Error>
+    CalcDistanceById(const float* vector, int64_t id) const override {
+        SAFE_CALL(return this->calc_distance_by_id(vector, id);)
+    };
+
+    tl::expected<BinarySet, Error>
+    Serialize() const override {
+        SAFE_CALL(return this->serialize();)
+    }
+
+    tl::expected<void, Error>
+    Serialize(std::ostream& out_stream) override {
+        SAFE_CALL(return this->serialize(out_stream);)
+    }
+
+    tl::expected<void, Error>
+    Deserialize(std::istream& in_stream) override {
+        SAFE_CALL(return this->deserialize(in_stream));
+    }
+
+    tl::expected<void, Error>
+    Deserialize(const BinarySet& binary_set) override {
+        SAFE_CALL(return this->deserialize(binary_set));
+    };
+
+    tl::expected<void, Error>
+    Deserialize(const ReaderSet& reader_set) override {
+        SAFE_CALL(return this->deserialize(reader_set);)
+    }
+
+    int64_t
+    GetNumElements() const override {
+        return this->basic_flatten_codes_->TotalCount();
+    }
+
+    // TODO(LHT): implement
+    int64_t
+    GetMemoryUsage() const override {
+        return 0;
+    }
+
+    void
+    SetBuildThreadsCount(uint64_t count) {
+        this->build_thread_count_ = count;
+        this->build_pool_->set_pool_size(count);
+    }
+
+    // TODO(LHT): make private
+    void
+    serialize(StreamWriter& writer) const;
+
+    void
+    deserialize(StreamReader& reader);
+
+public:
+    std::shared_ptr<FlattenInterface> basic_flatten_codes_{nullptr};
+    std::shared_ptr<FlattenInterface> high_precise_codes_{nullptr};
+    Vector<GraphInterfacePtr> route_graphs_;
+    GraphInterfacePtr bottom_graph_{nullptr};
+
+    bool use_reorder_{false};
+
+    int64_t dim_{0};
+    MetricType metric_{MetricType::METRIC_TYPE_L2SQR};
+
+    const nlohmann::json index_param_{};
+    const IndexCommonParam common_param_{};
+
+private:
+    tl::expected<std::vector<int64_t>, Error>
+    build(const DatasetPtr& data);
+
+    tl::expected<std::vector<int64_t>, Error>
+    add(const DatasetPtr& data);
+
+    tl::expected<DatasetPtr, Error>
+    knn_search(const DatasetPtr& query,
+               int64_t k,
+               const std::string& parameters,
+               const std::function<bool(int64_t)>& filter) const;
+
+    tl::expected<void, Error>
+    serialize(std::ostream& out_stream) const;
+
+    tl::expected<BinarySet, Error>
+    serialize() const;
+
+    tl::expected<void, Error>
+    deserialize(const ReaderSet& reader_set);
+
+    tl::expected<void, Error>
+    deserialize(const BinarySet& binary_set);
+
+    tl::expected<void, Error>
+    deserialize(std::istream& in_stream);
+
+    tl::expected<float, Error>
+    calc_distance_by_id(const float* vector, int64_t id) const;
+
+    inline int
+    get_random_level() {
+        std::uniform_real_distribution<double> distribution(0.0, 1.0);
+        double r = -log(distribution(level_generator_)) * mult_;
+        return (int)r;
+    }
+
+    void
+    hnsw_add(const DatasetPtr& data);
+
+    GraphInterfacePtr
+    generate_one_route_graph();
+
+    MaxHeap
+    search_one_graph(const float* query,
+                     const GraphInterfacePtr& graph,
+                     const std::shared_ptr<FlattenInterface>& codes,
+                     InnerIdType ep,
+                     uint64_t ef,
+                     BaseFilterFunctor* is_id_allowed = nullptr) const;
+
+    void
+    select_edges_by_heuristic(MaxHeap& edges,
+                              uint64_t max_size,
+                              const std::shared_ptr<FlattenInterface>& flatten);
+
+    InnerIdType
+    mutually_connect_new_element(InnerIdType cur_c,
+                                 MaxHeap& top_candidates,
+                                 GraphInterfacePtr graph,
+                                 std::shared_ptr<FlattenInterface> flatten,
+                                 bool is_update);
+
+    void
+    serialize_basic_info(StreamWriter& writer) const;
+
+    void
+    deserialize_basic_info(StreamReader& reader);
+
+    uint64_t
+    cal_serialize_size() const;
+
+private:
+    std::default_random_engine level_generator_{2021};
+    double mult_{1.0};
+
+    Allocator* allocator_{nullptr};
+
+    UnorderedMap<LabelType, InnerIdType> label_lookup_;
+    mutable std::shared_mutex label_lookup_mutex_{};  // lock for label_lookup_
+
+    InnerIdType entry_point_id_{std::numeric_limits<InnerIdType>::max()};
+    uint64_t max_level_{0};
+
+    uint64_t ef_construct_{400};
+    std::mutex global_mutex_;
+
+    // Locks operations with element by label value
+    mutable vsag::Vector<std::mutex> label_op_mutex_;
+
+    static const uint64_t MAX_LABEL_OPERATION_LOCKS = 65536;
+
+    std::shared_ptr<hnswlib::VisitedListPool> pool_{nullptr};
+
+    mutable vsag::Vector<std::shared_mutex> neighbors_mutex_;
+
+    std::unique_ptr<progschj::ThreadPool> build_pool_{nullptr};
+    uint64_t build_thread_count_{100};
+};
+}  // namespace vsag

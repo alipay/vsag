432a1d8c42f87ef2789f053bbb4c37535167baa3
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2024-12-17 22:56:02 +0800

    modify
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- examples/cpp/simple_pyramid.cpp
+++ examples/cpp/simple_pyramid.cpp
@@ -34,9 +34,9 @@ create_random_string(bool is_full) {
     std::vector<std::string> selected_levels;
 
     if (is_full) {
-        selected_levels.push_back(level1[rand() % level1.size()]);
-        selected_levels.push_back(level2[rand() % level2.size()]);
-        selected_levels.push_back(level3[rand() % level3.size()]);
+        selected_levels.emplace_back(level1[rand() % level1.size()]);
+        selected_levels.emplace_back(level2[rand() % level2.size()]);
+        selected_levels.emplace_back(level3[rand() % level3.size()]);
     } else {
         std::uniform_int_distribution<int> dist(1, 3);
         int num_levels = dist(mt);

cf12139bda5ed271b3ef7b903abaabdc86587602
inabao <37021995+inabao@users.noreply.github.com>
2024-12-18 10:57:22 +0800

    define the interface for the "pyramid" (#167)
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

---
+++ src/index/pyramid.h
@@ -0,0 +1,80 @@
+
+// Copyright 2024-present the vsag project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "vsag/index.h"
+
+namespace vsag {
+
+class Pyramid : public Index {
+public:
+    Pyramid() {
+    }
+
+    ~Pyramid() override = default;
+
+    tl::expected<std::vector<int64_t>, Error>
+    Build(const DatasetPtr& base) override;
+
+    tl::expected<DatasetPtr, Error>
+    KnnSearch(const DatasetPtr& query,
+              int64_t k,
+              const std::string& parameters,
+              BitsetPtr invalid = nullptr) const override;
+
+    tl::expected<DatasetPtr, Error>
+    KnnSearch(const DatasetPtr& query,
+              int64_t k,
+              const std::string& parameters,
+              const std::function<bool(int64_t)>& filter) const override;
+
+    tl::expected<DatasetPtr, Error>
+    RangeSearch(const DatasetPtr& query,
+                float radius,
+                const std::string& parameters,
+                int64_t limited_size = -1) const override;
+
+    tl::expected<DatasetPtr, Error>
+    RangeSearch(const DatasetPtr& query,
+                float radius,
+                const std::string& parameters,
+                BitsetPtr invalid,
+                int64_t limited_size = -1) const override;
+
+    tl::expected<DatasetPtr, Error>
+    RangeSearch(const DatasetPtr& query,
+                float radius,
+                const std::string& parameters,
+                const std::function<bool(int64_t)>& filter,
+                int64_t limited_size = -1) const override;
+
+    tl::expected<BinarySet, Error>
+    Serialize() const override;
+
+    tl::expected<void, Error>
+    Deserialize(const BinarySet& binary_set) override;
+
+    tl::expected<void, Error>
+    Deserialize(const ReaderSet& reader_set) override;
+
+    int64_t
+    GetNumElements() const override;
+
+    int64_t
+    GetMemoryUsage() const override;
+};
+
+}  // namespace vsag
